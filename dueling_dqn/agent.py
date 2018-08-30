27628e3031c66fa5e625c49998dcb2d6cb2b7441
Slavo Matasovsky <slavo.matas@gmail.com>
2018-08-30 19:55:41 +0200

    added training method for dueling dqn agent

--- dueling_dqn/agent.py
+++ dueling_dqn/agent.py
@@ -1,11 +1,28 @@
+import random
 import numpy as np
 
 import torch
 import torch.nn as nn
+import torch.optim as optim
 
-class DuelingDQN(nn.Module):
+import sys
+sys.path.append("../replay_buffer")
+
+from replay_buffer import PrioritizedReplayBuffer, LinearSchedule
+
+BUFFER_SIZE = int(1e5)  # replay buffer size
+BATCH_SIZE = 64  # minibatch size
+GAMMA = 0.99  # discount factor
+TAU = 1e-3  # for soft update of target parameters
+LR = 5e-4  # learning rate
+UPDATE_EVERY = 4  # how often to update the network
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class DuelingQNetwork(nn.Module):
     def __init__(self, input_shape, n_actions):
-        super(DuelingDQN, self).__init__()
+        super(DuelingQNetwork, self).__init__()
 
         self.conv = nn.Sequential(
             nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
@@ -40,3 +57,120 @@ class DuelingDQN(nn.Module):
         return val + adv - adv.mean()
 
 
+class Agent():
+    """Interacts with and learns from the environment."""
+
+    def __init__(self, input_shape, action_size, seed):
+        """Initialize an Agent object.
+
+        Params
+        ======
+            state_size (int): dimension of each state
+            action_size (int): dimension of each action
+            seed (int): random seed
+        """
+        self.state_size = input_shape
+        self.action_size = action_size
+        self.seed = random.seed(seed)
+
+        # Q-Network
+        self.qnetwork_local = DuelingQNetwork(input_shape, action_size).to(device)
+        self.qnetwork_target = DuelingQNetwork(input_shape, action_size).to(device)
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+
+        self.prioritized_replay_alpha = 0.6
+        self.prioritized_replay_beta0 = 0.4
+        self.prioritized_replay_beta_iters = 100000
+
+        # Replay memory
+        self.memory = PrioritizedReplayBuffer(BUFFER_SIZE, alpha=self.prioritized_replay_alpha)
+        self.beta_schedule = LinearSchedule(self.prioritized_replay_beta_iters,
+                                            initial_p=self.prioritized_replay_beta0,
+                                            final_p=1.0)
+
+        # Initialize time step (for updating every UPDATE_EVERY steps)
+        self.t_step = 0
+
+    def step(self, state, action, reward, next_state, done):
+        # Save experience in replay memory
+        self.memory.add(state, action, reward, next_state, done)
+
+        # Learn every UPDATE_EVERY time steps.
+        self.t_step = (self.t_step + 1) % UPDATE_EVERY
+        if self.t_step == 0:
+            # If enough samples are available in memory, get random subset and learn
+            if len(self.memory) > BATCH_SIZE:
+                experiences = self.memory.sample(BATCH_SIZE, beta=self.beta_schedule.value(len(self.memory)))
+                self.learn(experiences, GAMMA)
+
+    def act(self, state, eps=0.):
+        """Returns actions for given state as per current policy.
+
+        Params
+        ======
+            state (array_like): current state
+            eps (float): epsilon, for epsilon-greedy action selection
+        """
+        # state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+
+        self.qnetwork_local.eval()
+        with torch.no_grad():
+            action_values = self.qnetwork_local(state)
+        self.qnetwork_local.train()
+
+        # Epsilon-greedy action selection
+        if random.random() > eps:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_size))
+
+    def learn(self, experiences, gamma):
+        """Update value parameters using given batch of experience tuples.
+
+        Params
+        ======
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples
+            gamma (float): discount factor
+        """
+        # print("learn")
+
+        states, actions, rewards, next_states, dones, weights, idxes = experiences
+
+        # Get max predicted Q values (for next states) from target model
+        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        # Compute Q targets for current states
+        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+
+        # Get expected Q values from local model
+        Q_expected = self.qnetwork_local(states).gather(1, actions)
+
+        # Compute loss
+        losses_v = weights * (Q_expected - Q_targets) ** 2
+        loss = losses_v.mean()
+        prios = losses_v + 1e-5
+
+        # Minimize the loss
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        # Update replay buffer priorities
+        self.memory.update_priorities(idxes, prios.data.cpu().numpy())
+
+        # ------------------- update target network ------------------- #
+        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
+
+    def soft_update(self, local_model, target_model, tau):
+        """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+
+        Params
+        ======
+            local_model (PyTorch model): weights will be copied from
+            target_model (PyTorch model): weights will be copied to
+            tau (float): interpolation parameter
+        """
+        # print("soft_update")
+
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
