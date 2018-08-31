637a366ea38b9dd2604533604f9f9b3825267f7e
Slavo Matasovsky <slavo.matas@gmail.com>
2018-08-31 13:52:08 +0200

    Distributional dqn agent updates

--- categorical_dqn/agent.py
+++ categorical_dqn/agent.py
@@ -148,27 +148,43 @@ class Agent():
         # print("learn")
 
         states, actions, rewards, next_states, dones, weights, idxes = experiences
+        #states, actions, rewards, dones, next_states = common.unpack_batch(batch)
+        batch_size = len(states)
 
-        # Get max predicted Q values (for next states) from target model
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
-        # Compute Q targets for current states
-        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+        states_v = torch.tensor(states).to(device)
+        actions_v = torch.tensor(actions).to(device)
+        next_states_v = torch.tensor(next_states).to(device)
 
-        # Get expected Q values from local model
-        Q_expected = self.qnetwork_local(states).gather(1, actions)
+        # next state distribution
+        next_distr_v, next_qvals_v = self.qnetwork_target.both(next_states_v)
+        next_actions = next_qvals_v.max(1)[1].data.cpu().numpy()
+        next_distr = self.qnetwork_target.apply_softmax(next_distr_v).data.cpu().numpy()
 
-        # Compute loss
-        losses_v = weights * (Q_expected - Q_targets) ** 2
-        loss = losses_v.mean()
-        prios = losses_v + 1e-5
+        next_best_distr = next_distr[range(batch_size), next_actions]
+        dones = dones.astype(np.bool)
+
+        # project our distribution using Bellman update
+        proj_distr = self.distr_projection(next_best_distr, rewards, dones, Vmin, Vmax, N_ATOMS, gamma)
+
+        # calculate net output
+        distr_v = self.qnetwork_local(states_v)
+        state_action_values = distr_v[range(batch_size), actions_v.data]
+        state_log_sm_v = F.log_softmax(state_action_values, dim=1)
+        proj_distr_v = torch.tensor(proj_distr).to(device)
+
+        """
+        if save_prefix is not None:
+            pred = F.softmax(state_action_values, dim=1).data.cpu().numpy()
+            save_transition_images(batch_size, pred, proj_distr, next_best_distr, dones, rewards, save_prefix)
+        """
+
+        loss_v = -state_log_sm_v * proj_distr_v
+        loss_v = loss_v.sum(dim=1).mean()
 
         # Minimize the loss
         self.optimizer.zero_grad()
-        loss.backward()
+        loss_v.backward()
         self.optimizer.step()
-
-        # Update replay buffer priorities
-        self.memory.update_priorities(idxes, prios.data.cpu().numpy())
 
         # ------------------- update target network ------------------- #
         self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
@@ -187,3 +203,41 @@ class Agent():
 
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
+
+
+    def distr_projection(next_distr, rewards, dones, Vmin, Vmax, n_atoms, gamma):
+        """
+        Perform distribution projection aka Catergorical Algorithm from the
+        "A Distributional Perspective on RL" paper
+        """
+        batch_size = len(rewards)
+        proj_distr = np.zeros((batch_size, n_atoms), dtype=np.float32)
+        delta_z = (Vmax - Vmin) / (n_atoms - 1)
+        for atom in range(n_atoms):
+            tz_j = np.minimum(Vmax, np.maximum(Vmin, rewards + (Vmin + atom * delta_z) * gamma))
+            b_j = (tz_j - Vmin) / delta_z
+            l = np.floor(b_j).astype(np.int64)
+            u = np.ceil(b_j).astype(np.int64)
+            eq_mask = u == l
+            proj_distr[eq_mask, l[eq_mask]] += next_distr[eq_mask, atom]
+            ne_mask = u != l
+            proj_distr[ne_mask, l[ne_mask]] += next_distr[ne_mask, atom] * (u - b_j)[ne_mask]
+            proj_distr[ne_mask, u[ne_mask]] += next_distr[ne_mask, atom] * (b_j - l)[ne_mask]
+        if dones.any():
+            proj_distr[dones] = 0.0
+            tz_j = np.minimum(Vmax, np.maximum(Vmin, rewards[dones]))
+            b_j = (tz_j - Vmin) / delta_z
+            l = np.floor(b_j).astype(np.int64)
+            u = np.ceil(b_j).astype(np.int64)
+            eq_mask = u == l
+            eq_dones = dones.copy()
+            eq_dones[dones] = eq_mask
+            if eq_dones.any():
+                proj_distr[eq_dones, l] = 1.0
+            ne_mask = u != l
+            ne_dones = dones.copy()
+            ne_dones[dones] = ne_mask
+            if ne_dones.any():
+                proj_distr[ne_dones, l] = (u - b_j)[ne_mask]
+                proj_distr[ne_dones, u] = (b_j - l)[ne_mask]
+        return proj_distr
