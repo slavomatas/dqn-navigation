e8e0092ccf8d3278e2cc6392a36d5a2a3d741299
Slavo Matasovsky <slavo.matas@gmail.com>
2018-11-21 10:36:48 +0100

    Basic DQN agent - code formatting

--- basic_dqn/agent.py
+++ basic_dqn/agent.py
@@ -118,7 +118,8 @@ class Agent():
 
         # Get max predicted Q values (for next states) from target model
         Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
-        # Compute Q targets for current states 
+
+        # Compute Q targets for current states
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
         # Get expected Q values from local model
@@ -126,6 +127,7 @@ class Agent():
 
         # Compute loss
         loss = F.mse_loss(Q_expected, Q_targets)
+
         # Minimize the loss
         self.optimizer.zero_grad()
         loss.backward()
